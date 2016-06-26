14e4172ee9c253ecc6d5d023b44700518c09d629
Steven Petryk <me@stevenpetryk.com>
2016-06-26 00:08:02 -0400

    Initial commit

---
+++ spec/atom-spexy-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomSpexyView from '../lib/atom-spexy-view';
+
+describe('AtomSpexyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
